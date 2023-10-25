f929e48ae0c26301a32159a75abb356a052dabfb
Jefry Zarate Ruiz <jefry.zarate@gco.com.co>
2023-10-25 14:33:07 -0500

    Data

---
+++ cat/main.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import axios from 'axios'
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+  const users = await axios.get("http://service-user:3000/users").then(req => req.data)
+  
+  res.json({
+    users,
+    pet: {
+      name: "Nieve"
+    }
+  })
+})
+
+app.listen(3001, () => {
+  console.log("Is running")
+})
