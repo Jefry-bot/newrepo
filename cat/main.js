33f93933e5d736dc53f9508c0f06f2fcdc76a6fd
Jefry Zarate Ruiz <jefry.zarate@gco.com.co>
2023-10-26 08:30:21 -0500

    Change pet name

--- cat/main.js
+++ cat/main.js
@@ -1,15 +1,16 @@
 import express from 'express'
-import axios from 'axios'
 
 const app = express()
 
 app.use(express.json())
 
 app.get("/", async (req, res) => {
-  const users = await axios.get("http://service-user:3000/users").then(req => req.data)
-  
   res.json({
-    users,
+    users: [
+      {
+        name: "Alawaza"
+      }
+    ],
     pet: {
       name: "Izi"
     }
